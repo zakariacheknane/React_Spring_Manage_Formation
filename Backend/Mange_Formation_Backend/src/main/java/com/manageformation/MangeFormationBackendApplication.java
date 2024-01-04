2355128f5c5ea42f2d4c9c016202c1846eb8cb5f
zakariacheknane <cheknanezakaria@gmail.com>
2024-01-04 23:31:51 +0100

    Add Backend

---
+++ Backend/Mange_Formation_Backend/src/main/java/com/manageformation/MangeFormationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manageformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangeFormationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangeFormationBackendApplication.class, args);
+	}
+
+}
