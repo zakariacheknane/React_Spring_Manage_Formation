974296183818ccceb99caa0d989848641fffc3b6
zakariacheknane <cheknanezakaria@gmail.com>
2024-01-14 03:49:42 +0100

    Create sidebar and simple Dashboard

--- Frontend/Mange_Formation_React/src/App.js
+++ Frontend/Mange_Formation_React/src/App.js
@@ -1,18 +1,27 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+
 function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
-       <ThemeProvider theme={theme}>
-       <CssBaseline />
-    <div className="app">
-    <main className="content">
-     <Topbar/>
-    </main>
-    </div>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar/>
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+  
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
---
+++ Frontend/Mange_Formation_React/src/scenes/dashboard/index.jsx
@@ -0,0 +1,8 @@
+
+const Dashboard = () => {
+  return (
+    <div>welcome to dashboard</div>
+  )
+}
+
+export default Dashboard
